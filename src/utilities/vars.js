5ed66b98bbcc5ebe3d46160a7d8ac4d077ca2eba
Andrew <andrewapinon@gmail.com>
2020-08-03 07:03:32 +0800

    dslkjf

--- src/utilities/vars.js
+++ src/utilities/vars.js
@@ -1,4 +1,10 @@
-export const loginEndpoint = "http://localhost:5000/login/admin";
-export const createAdmin = "http://localhost:5000/admin/createAdmin";
-export const createSubjectEndpoint = "http://localhost:5000/admin/createSubject"
-export const createProspectusEndpoint = "http://localhost:5000/admin/createProspectus"
+// export const loginEndpoint = "http://localhost:5000/login/admin";
+// export const createAdmin = "http://localhost:5000/admin/createAdmin";
+// export const createSubjectEndpoint = "http://localhost:5000/admin/createSubject"
+// export const createProspectusEndpoint = "http://localhost:5000/admin/createProspectus"
+
+//FOR PRODUCTION
+export const loginEndpoint = "https://pit-api.herokuapp.com/login/admin";
+export const createAdmin = "https://pit-api.herokuapp.com/admin/createAdmin";
+export const createSubjectEndpoint = "https://pit-api.herokuapp.com/admin/createSubject"
+export const createProspectusEndpoint = "https://pit-api.herokuapp.com/admin/createProspectus"
